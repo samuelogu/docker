03162a3b4cd015e30bfc1c905e74c906124afde6
Samuel Ogu <hello@samuelogu.com>
2020-11-10 11:33:08 +0100

    dotenv setup

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-const MONGO_USERNAME = 'sammy';
-const MONGO_PASSWORD = 'psalm294u';
-const MONGO_HOSTNAME = '127.0.0.1';
-const MONGO_PORT = '27017';
-const MONGO_DB = 'docker';
+const MONGO_USERNAME = process.env.MONGO_USERNAME
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD
+const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME
+const MONGO_PORT = process.env.MONGO_PORT
+const MONGO_DB = process.env.MONGO_DB;
 
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
