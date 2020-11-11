2568ca1a8ccba11d423c9c314d7dc063fa1da2ba
Samuel Ogu <hello@samuelogu.com>
2020-11-11 09:47:48 +0100

    db

--- db.js
+++ db.js
@@ -7,8 +7,9 @@ const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME
 const MONGO_PORT = process.env.MONGO_PORT
 const MONGO_DB = process.env.MONGO_DB;
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+// const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const url = 'mongodb://localhost:27017/account'
 
-console.log(url);
+// console.log(url);
 
 mongoose.connect(url, {useNewUrlParser: true})
