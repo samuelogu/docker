a99bad1aadeedf174079346f722fe8dcaeea127b
Samuel Ogu <hello@samuelogu.com>
2020-11-12 22:58:34 +0100

    checking without mongo

--- app.js
+++ app.js
@@ -9,17 +9,30 @@ app.use(data.array());
 
 app.use(bodyParser.json())
 
-const db = require('./db');
+// const db = require('./db');
+const mongoose = require('mongoose');
 const customer = require('./controllers/customers')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/posts', (req, res) => {
+    res.send('posts route!')
+})
+
 app.get('/customers', customer.get)
 
 app.post('/customers', customer.create)
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+const url = 'mongodb://mongo:27018/account'
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log('Mongodb connected!');
+    app.listen(port, () => {
+        console.log('Testing!');
+        console.log(`Example app listening at http://localhost:${port}`)
+    })
+}).catch(err => {
+    console.log(err);
 })
--- db.js
+++ db.js
@@ -7,8 +7,8 @@ const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME
 const MONGO_PORT = process.env.MONGO_PORT
 const MONGO_DB = process.env.MONGO_DB;
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-// const url = 'mongodb://localhost:27017/account'
+// const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const url = 'mongodb://mongo:27017/account'
 
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log('Mongodb connected!');
