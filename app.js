6d381ab093c8b1192537abc955fa4fa1d1624730
Samuel Ogu <hello@samuelogu.com>
2020-11-12 23:38:07 +0100

    dockerfile build

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 3000
 const bodyParser = require('body-parser')
 
 const multer = require('multer');
@@ -25,7 +25,7 @@ app.get('/customers', customer.get)
 
 app.post('/customers', customer.create)
 
-const url = 'mongodb://mongo:27017/account'
+const url = 'mongodb://mongo:27117/account'
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
